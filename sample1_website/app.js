253ed05f789f4e4cce3898a0ed867768f4c267c4
Syed Abdul  Muqeeth <54002413+theheld@users.noreply.github.com>
2022-11-19 13:15:01 +0530

    Sample Protfolio 1
    simple website design to show case my skills ntg fancy!

---
+++ sample1_website/app.js
@@ -0,0 +1,20 @@
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
